8c0c74809b974f1b374ae592ab4688d6a9038a5c
cungtu <ceeter1999@gmail.com>
2024-05-14 18:23:56 +0700

    len

---
+++ src/test/java/com/server/anhminh/AnhMinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.anhminh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnhMinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
